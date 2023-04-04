cc59ea4a92d1f61cbd98fd298651077f474b5623
MatheusDLins <matheus934qwer@gmail.com>
2023-04-04 16:11:13 -0300

    Iniciando bootcamp

---
+++ 01 - Primeiras Páginas Interativas com JavaScript/01.1 - Primeiros passos com JavaScript/aula.js
@@ -0,0 +1,11 @@
+//Imprimir no console do navegador
+
+console.log('Olá, Mundo!');
+
+//Maneiras de declarar variaveis, "let" o valor pode mudar ao longo do codigo enquanto "const" o valor não pode ser alterado
+
+let variavel = 10;
+
+variavel = 20;
+
+const pi = 3.14;
